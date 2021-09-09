803a939905cc2181ae74aa3b0e8faa8b9c6540d3
Tyler <tygary@gmail.com>
2021-09-09 14:40:38 -0700

    Changing deployment to work with heroku

--- packages/client/src/util/api.js
+++ packages/client/src/util/api.js
@@ -1,5 +1,5 @@
 import React from "react";
-const API_URL = "http://localhost:3000/api";
+const API_URL = "http://localhost:5000/api";
 
 export const useFetch = (url, options = {}) => {
   const [response, setResponse] = React.useState(null);
--- packages/server/config/keys.js
+++ packages/server/config/keys.js
@@ -6,7 +6,7 @@ module.exports = {
     apiEndpoint: (process.env.API_URL) ? `/${process.env.API_URL}` : '/api',
   },
   database: {
-    url: process.env.MONGODB_URI || 'mongodb://localhost:27017/' + repoName,
+    url: "mongodb+srv://dbUser:VRKwfLgC3zCaWbPM@cluster0.yzek9.mongodb.net/Cluster0?retryWrites=true&w=majority" //process.env.MONGODB_URI || 'mongodb://localhost:27017/' + repoName,
   },
   jwt: {
     secret: process.env.JWT_SECRET || 'jwt-secret',
