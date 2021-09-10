89e2928a57eefcbdee1435e01d2f607ccb872ebd
Mia Peace <miapeace@Kenzies-MacBook-Pro.local>
2021-09-10 13:16:08 -0800

    added mongodb connect

--- packages/server/src/config/keys.js
+++ packages/server/src/config/keys.js
@@ -1,14 +1,14 @@
 module.exports = {
   app: {
     name: "MERN App",
-    apiEndpoint: (process.env.API_URL) ? `/${process.env.API_URL}` : '/api',
+    apiEndpoint: process.env.API_URL ? `/${process.env.API_URL}` : "/api",
   },
   database: {
-    url: process.env.MONGODB_URI || 'mongodb://localhost:27017/' + repoName, // for local mongodb
+    url: `mongodb+srv://mxsilentwar:103117Dp!@teaching.sll1f.mongodb.net/testerson?retryWrites=true&w=majority`, // for local mongodb
     // url: "PUT-YOUR-MONGODB-CONNECTION-STRING-HERE" // for mongodb atlas, comment the above line, uncomment this line, and add your connection string.
   },
   jwt: {
-    secret: process.env.JWT_SECRET || 'jwt-secret',
-    tokenLife: '7d',
+    secret: process.env.JWT_SECRET || "jwt-secret",
+    tokenLife: "7d",
   },
-}
+};
